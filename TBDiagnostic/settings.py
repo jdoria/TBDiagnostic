1a89624b4b19f8fad59b59a88156e5ed71aaedd0
Juan Doria <jdoria@uan.edu.co>
2018-05-12 19:51:29 -0500

    Se lee la llave aplicativa desde archivo.

--- TBDiagnostic/settings.py
+++ TBDiagnostic/settings.py
@@ -22,7 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(-s3!6r-z!wi11$6rwasnj$yjcem4gx)pm+hk&9ajvr4ba!qgx'
+#SECRET_KEY = '(-s3!6r-z!wi11$6rwasnj$yjcem4gx)pm+hk&9ajvr4ba!qgx'
+
+with open('./secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
